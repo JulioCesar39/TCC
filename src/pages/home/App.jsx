7dad10f576b197dc66be58ee86b164c1560d0714
JulioCesar39 <ra47846328832@acaonsfatima.org.br>
2024-10-29 09:42:57 -0300

    cabeçalho on

---
+++ src/pages/home/App.jsx
@@ -0,0 +1,26 @@
+import './App.scss';
+
+export default function App() {
+  return (
+    <div className="pagina-inicial">
+      <header className="cabeçalho">
+       <img src="./assets/image/logo.png" alt="logo" />
+       <nav>
+        <li>CONTATO</li>
+        <li>AGENDAMENTO</li>
+        <li>SERVIÇOS</li>
+        <li>PORTFÓLIO</li>
+       </nav>
+      <div className='menu'>
+      <img src="./assets/image/menu.png" alt="" />
+      </div>
+       
+      
+      </header>
+    
+    </div>
+    
+  );
+}
+
+
