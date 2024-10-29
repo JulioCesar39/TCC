d03919b2ef306eac6dcb9d0db143fb13c55b4b1b
JulioCesar39 <ra47846328832@acaonsfatima.org.br>
2024-10-29 07:49:00 -0300

    subindo no git

---
+++ src/pages/home/App.js
@@ -0,0 +1,13 @@
+import '../home/App.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>TCC</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
