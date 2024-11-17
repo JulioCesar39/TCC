f41a52cd94b7a43c03ef06112525b03508f7b886
Dacruz2 <158165860+Dacruz2@users.noreply.github.com>
2024-11-17 20:00:06 -0300

    Add files via upload

---
+++ servicosDeOrientacaoVocacional/index.jsx
@@ -0,0 +1,57 @@
+import './index.scss';
+
+import { Link } from 'react-router-dom';
+
+
+export default function ServicosDeOrientacaoVocacional() {
+
+
+  return (
+
+    <div className="pagina-servico">
+    <header className="cabeçalho">
+      <img src="./assets/image/logo.png" alt="logo" className="logo" />
+      <nav className="menu-opcoes">
+        <li><Link to={'/'}>HOME</Link></li>
+        <li><Link>CONTATO</Link></li>
+        <li><Link>AGENDAMENTO</Link></li>
+        <li><Link>PORTFÓLIO</Link></li>
+      </nav>
+    </header>
+  
+
+      <div className='container'>
+
+        <img src="/assets/image/fundo-azul.png" alt="" />
+
+        <h1 classname='titulo'>serviços de orientação vocacional</h1>
+
+        <main className="content">
+
+          <div className='card'>
+            <p>
+            SERVIÇOS DE ORIENTAÇÃO VOCACIONAL AJUDAM A IDENTIFICAR INTERESSES E HABILIDADES POR MEIO DE AVALIAÇÕES, EXPLORAM OPÇÕES DE CARREIRA E OFERECEM SUPORTE NA ELABORAÇÃO DE CURRÍCULOS E PREPARAÇÃO PARA ENTREVISTAS. INCLUEM PLANEJAMENTO DE CARREIRA, SUPORTE EMOCIONAL E ACOMPANHAMENTO. ESTÃO DISPONÍVEIS EM ESCOLAS, UNIVERSIDADES E CENTROS DE CARREIRA.
+            </p>
+          </div>
+          <div className="card1">
+          <img src="./assets/image/perfil.png" alt="" />
+            <h3>Dr. Larissa de Jesus</h3>
+            <ul>
+              <li>Consulta Online</li>
+              <li>Consulta Presencial</li>
+              <li>1ª Consulta - Valor de R$130,00</li>
+            </ul>
+          </div>
+          <div className='botao'><button>Agende Aqui</button></div>  
+        </main>
+
+
+
+
+
+
+      </div>
+
+    </div>
+  )
+}
---
+++ servicosPsicoterapiaIndividual/index.jsx
@@ -0,0 +1,61 @@
+import './index.scss';
+
+import { Link } from 'react-router-dom';
+
+
+export default function ServicosPsicoterapiaIndividual() {
+
+
+  return (
+
+   
+        <div className="pagina-servico">
+    <header className="cabeçalho">
+      <img src="./assets/image/logo.png" alt="logo" className="logo" />
+      <nav className="menu-opcoes">
+        <li><Link to={'/'}>HOME</Link></li>
+        <li><Link>CONTATO</Link></li>
+        <li><Link>AGENDAMENTO</Link></li>
+        <li><Link>PORTFÓLIO</Link></li>
+      </nav>
+    </header>
+  
+
+
+ 
+
+      <div className='container'>
+
+        <img src="/assets/image/fundo-azul.png" alt="" />
+
+        <h1 classname='titulo'>Psicoterapia individual para adolescentes e adultos na abordagem psicanalítica</h1>
+
+        <main className="content">
+
+          <div className='card'>
+            <p>
+            A PSICOTERAPIA PSICANALÍTICA INDIVIDUAL EXPLORA O INCONSCIENTE E AS EMOÇÕES, FOCANDO NA HISTÓRIA PESSOAL DO PACIENTE, NA INTERPRETAÇÃO DE SONHOS E NA TRANSFERÊNCIA DE SENT- IMENTOS. TRABALHA COM CONFLITOS INTERNOS, COMO MEDOS E ANSIEDADES, E VISA AUMENTAR A AUTOCONSCIÊNCIA. O AMB- IENTE TERAPÊUTICO OFERECE SEGURANÇA PARA A EXPRESSÃO LIVRE, PROMOVENDO MUDANÇAS DURADOURAS.
+            </p>
+          </div>
+          <div className="card1">
+          <img src="./assets/image/perfil.png" alt="" />
+            <h3>Dr. Larissa de Jesus</h3>
+            <ul>
+              <li>Consulta Online</li>
+              <li>Consulta Presencial</li>
+              <li>1ª Consulta - Valor de R$130,00</li>
+            </ul>
+          </div>
+          <button> Agende Aqui</button>
+        </main>
+
+
+
+
+
+
+      </div>
+
+    </div>
+  )
+}
